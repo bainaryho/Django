174319b26d3959a4c2a669e257bcfe01e4b6b403
bainaryho <rkswl233@naver.com>
2023-06-26 11:17:21 +0900

    UPDATE BookmarkAdmin

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -7,5 +7,5 @@ from bookmark.models import Bookmark
 # 데코레이터 패턴을 사용하지 않는 방법
 #admin.site.regiter(Bookmark, BookmarkAdmin)
 @admin.register(Bookmark)
-class BookAdmin(admin.ModelAdmin):
+class BookmarkAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'url')
